2078ea133004d9aed8857d069607b91ee02c072e
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-06-24 17:11:20 -0400

    Finished Express

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+/* let db = [
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'This is the scream body',
+            createdAt: '2020-06-24T21:02:59.397Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+] */
