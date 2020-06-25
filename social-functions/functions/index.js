6edbfe530b05153cbc9144dacfcfa8d5c1735e2d
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-06-25 14:23:13 -0400

    User signup

--- social-functions/functions/index.js
+++ social-functions/functions/index.js
@@ -1,10 +1,21 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+const app = require('express')()
 
 admin.initializeApp()
 
-const express = require('express')
-const app = express()
+const config = {
+    apiKey: "AIzaSyC5m42y32HeN01tVfJxfzY8dd6SK7hFAE4",
+    authDomain: "social-25721.firebaseapp.com",
+    databaseURL: "https://social-25721.firebaseio.com",
+    projectId: "social-25721",
+    storageBucket: "social-25721.appspot.com",
+    messagingSenderId: "131712891010",
+    appId: "1:131712891010:web:284570974c750bbf9f1c9b"
+}
+
+const firebase = require('firebase')
+firebase.initializeApp(config)
 
 app.get('/screams', (req,res) => {
   admin
@@ -46,5 +57,25 @@ app.post('/scream', (req, res) => {
   })
 })
 
+// Signup Route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPassword: req.body.confirmedPassword,
+    handle: req.body.handle
+  }
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(data => {
+    return res
+    .status(201)
+    .json({ message: `user ${data.user.uid} signed up successfully`})
+  })
+  .catch((err) => {
+    res.status(500).json({ error: error.code})
+    console.error(err)
+  })
+})
 
 exports.api = functions.https.onRequest(app)
